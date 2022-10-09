321bdc950e1e2c860a0c2a7a0d4f0f2bf4e79354
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-10-10 02:50:47 +0500

    add 09,11

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 11. Напишите программу, которая
+// 1. выводит случайное трехзначное число и
+// 2. удаляет вторую цифру этого числа.
+
+// 456->46
+// 782->72
+// 918->98
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное число из диапозона 100 - 999 => {number}");
+
+int SecondDigit (int num)
+{
+    int firstDigit = num / 100;   // 456 / 100 = 4
+    int thirdDigit = num % 10;    // 456 % 10 = 6
+
+    return firstDigit * 10 + thirdDigit;
+}
+int result = SecondDigit(number);
+Console.WriteLine($"Без второй цифры числа = {result}");
