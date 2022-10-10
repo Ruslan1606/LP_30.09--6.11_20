676632315d05997de1db469eb7f856fbc67078d6
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-10-10 17:26:05 +0500

    add 12,14,16

---
+++ Task14/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 14. Напишите программу, которая
+// 1. принимает на вход число и
+// 2. проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> Нет
+// 46 -> Нет
+// 161 -> да
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool CheckingNumber(int number)
+{
+    return number % 7 == 0 && number % 23 == 0;
+}
+
+bool result = CheckingNumber(num);
+if (result)
+{
+    Console.Write("да");
+}
+else{Console.Write("нет");}
---
+++ Task16/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 16 Напишите программу, которая принимает на 
+// 1. вход два числа и проверяет, является ли
+// 2. одно число квадратом другого.
+
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool CheckingNumbers(int number1, int number2)
+// {
+//     int min = number1;
+//     int max = number2;
+//     if (number1 > number2)
+//     {
+//         min = number2;
+//         max = number1;
+//     }
+//     return min * min == max;
+// }
+
+// bool result = CheckingNumbers(num1, num2);
+// if (result) Console.Write("да");
+// else Console.Write("нет");
+{
+    return number1 * number1 == number2 || number2 * number2 == number1;
+}
+if (CheckingNumbers(num1, num2)) Console.WriteLine("да");
+else Console.WriteLine("нет");
