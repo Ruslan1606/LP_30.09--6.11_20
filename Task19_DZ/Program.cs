9480932235556c9840099110faa58c6d3127b769
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-10-15 03:43:28 +0500

    add 19

--- Task19_DZ/Program.cs
+++ Task19_DZ/Program.cs
@@ -10,28 +10,6 @@
 
 // 23432->да
 
-// using System;
-// class HelloWorld {
-//   static bool isPal(int n)
-//   {
-//       int r=0,nn=n;
-//       while (n>0)
-//       {
-//           r=r*10+n%10;
-//           n=n/10;
-//       }
-//       return r==nn;
-//   }
-//   static void Main() {
-//     int n;  
-//     Console.Write("n=");
-//     n=Convert.ToInt32(Console.ReadLine());
-//     if (isPal(n))
-//        Console.WriteLine("Палиндром");
-//     else   
-//        Console.WriteLine("Не палиндром");
-//   }
-// }
 
 
 // Console.WriteLine("Введите число: ");
@@ -53,8 +31,27 @@
 // {
 //     Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
 
+
+
 //  Вариант целочисленным делением
 
-
 Console.WriteLine("Введите число! ");
- int num = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >=10000 && number <=99999)
+{
+    if (number / 10000 == number % 10 && (number % 100) / 10 == (number % 1000) / 100)
+    {
+        Console.WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine($"{number} - Не является пятизначным числом");
+}
+
+// НЕ МОГУ ПОНЯТЬ ПОЧЕМУ НЕ ПРОХОДИТ!!!
