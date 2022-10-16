2172a42684c25f62f76058a64f44fede8e8d9993
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-10-17 03:03:19 +0500

    add 28 30

---
+++ Task28/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 28: Напишите программу, которая
+// 1.принимает на вход число N и 
+// 2.выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int factorial = Factorial(number);
+// Console.WriteLine($"Произведение чисел от 1 до {number} = {factorial} ");
+
+int Factorial(int num)
+{
+
+    int fact = 1;
+    int i;
+    for (i = 1; i <= number; i++)
+    {
+        try
+        {
+            checked
+            {
+                fact = fact * i;
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Переполнение типа");
+            break;
+        }
+
+    }
+    Console.WriteLine($"Факториал числа {i -1} = {fact}");
+    return fact;
+}
+
--- Task29_DZ/Program.cs
+++ Task29_DZ/Program.cs
@@ -4,4 +4,25 @@
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
+int[] FillArray(int num)
+{
+    int[] array = new int [num];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1, 20);
+    }
+    return array;
+}
 
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+
+int[] myArr= FillArray(8);
+PrintArray(myArr);
