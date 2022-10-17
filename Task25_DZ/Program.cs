d87ec61d9585e8e2e39d7e2659480203aa0533e2
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-10-17 10:44:28 +0500

    add 25

--- Task25_DZ/Program.cs
+++ Task25_DZ/Program.cs
@@ -6,4 +6,28 @@
 
 // 2, 4-> 16
 
+Console.WriteLine("Введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+
+if (A == 0)
+{
+  result = 0;
+}
+else if(B == 0)
+{
+  result = 1;
+}
+else // A != 0 && B != 0
+{
+  result = A;
+  for (int i = 0; i < B -1; i++)
+  {
+    result = result * A;
+  }
+}
+Console.WriteLine($"{A} в степени {B} = {result}");
 
