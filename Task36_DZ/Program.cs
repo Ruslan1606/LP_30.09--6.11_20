b774331401dbf3fc87a742bd26360ae006fb0952
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-10-22 23:45:05 +0500

    add 36

--- Task36_DZ/Program.cs
+++ Task36_DZ/Program.cs
@@ -7,4 +7,36 @@
 // [-4, -6, 89, 6] -> 0
 
 
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    var array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateArrayRndInt(5, 1, 10);
+int sum = 0;
+
+for (int i = 1; i < array.Length; i += 1)
+{
+    if (i % 2 != 0) sum += array[i];
+}
+PrintArray(array);
+Console.WriteLine($" -> {sum}");
 
