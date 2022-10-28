83284846d869e53d0b6d4673a40f9b891a2fa60f
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-10-28 10:13:41 +0500

    add 38

--- Task38_DZ/Program.cs
+++ Task38_DZ/Program.cs
@@ -4,4 +4,49 @@
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
 
+  double[] CreateArrayRndDouble(int size, double min, double max)
+{
+    var array = new double[size];
+    var rnd = new Random();
 
+    for (int i = 0; i < size; i++)
+    {
+        double rndNum = rnd.NextDouble() * (max - min) + min;
+        array[i] = Math.Round(rndNum, 2);
+    }
+    return array;
+}
+
+void PrintArrayDouble(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("] -> ");
+}
+
+double DiffArray(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+  	return max - min;
+}
+
+double[] arr1 = CreateArrayRndDouble(8, -9, 9);
+var result = DiffArray(arr1);
+PrintArrayDouble(arr1);
+Console.WriteLine($"{result}");
