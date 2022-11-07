6d5e04c6e099e0e0c58c1ad08fad8aaad9c24597
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-11-08 00:33:39 +0500

    add 41 43

--- Task43_DZ/Program.cs
+++ Task43_DZ/Program.cs
@@ -6,3 +6,19 @@
 // Пример
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите значение b1:");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1:");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2:");
+var b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2:");
+var k2 = Convert.ToDouble(Console.ReadLine());
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Точка пересечения двух прямых: ({x};{y})");
