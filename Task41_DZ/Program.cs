71837ce42a3a9bc68907ab28fe47e9be2846b25c
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-11-07 16:27:34 +0500

    add 41

--- Task41_DZ/Program.cs
+++ Task41_DZ/Program.cs
@@ -6,3 +6,27 @@
 // -1, -7, 567, 89, 223-> 3
 
 
+Console.WriteLine("Введите количество чисел:");
+string num;
+num = Console.ReadLine();
+int number;
+number = Convert.ToInt32(num);
+
+int[] numbers = new int[number];
+int count = 0;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+  num = Console.ReadLine();
+  number = Convert.ToInt32(num);
+  numbers[i] = number;
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > 0)
+    {
+    	count++;
+    }
+}
+Console.WriteLine($"Чисел больше > 0 = {count}");
