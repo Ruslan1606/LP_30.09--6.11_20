9a6cbd8ec1c29f78226fb36f3252d89786917fe6
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-11-08 02:39:35 +0500

    add 44 45

--- Task41_DZ/Program.cs
+++ Task41_DZ/Program.cs
@@ -6,27 +6,29 @@
 // -1, -7, 567, 89, 223-> 3     почему то объяснили на следующем семинаре 
 
 
-Console.WriteLine("Введите количество чисел:");
-string num;                           // Console.ReadLine().Split(",").Select(int.Parse);
-num = Console.ReadLine();
-int number;
-number = Convert.ToInt32(num);
+Console.WriteLine("Введите числа через запятую:");
+// string num;                           // Console.ReadLine().Split(",").Select(int.Parse);
+// num = Console.ReadLine();
+// int number;
+// number = Convert.ToInt32(num);
 
-int[] numbers = new int[number];
+// int[] numbers = new int[number];
+// int count = 0;
+
+// for(int i = 0; i < numbers.Length; i++)
+// {
+//   num = Console.ReadLine();
+//   number = Convert.ToInt32(num);
+//   numbers[i] = number;
+// }
+int[] number = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
 int count = 0;
 
-for(int i = 0; i < numbers.Length; i++)
+for (int i = 0; i < number.Length; i++)
 {
-  num = Console.ReadLine();
-  number = Convert.ToInt32(num);
-  numbers[i] = number;
-}
-
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > 0)
+    if (number[i] > 0)
     {
     	count++;
     }
 }
-Console.WriteLine($"Чисел больше > 0 = {count}");
+Console.WriteLine($"Чисел больше > 0 = {count} ");
