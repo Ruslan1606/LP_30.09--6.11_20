5b6ae31c391cbb2ef9e11615d741b2c746402a4d
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-11-14 14:42:09 +0500

    add 45

--- Task52_DZ/Program.cs
+++ Task52_DZ/Program.cs
@@ -7,3 +7,53 @@
 // 8 4 2 4
 
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    var matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void MidColumnMatrix(int[,] array2D)
+{
+    for (int j = 0; j < array2D.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < array2D.GetLength(0); i++)
+        {
+            sum += array2D[i, j];
+        }
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+            else Console.Write($"{matrix[i, j], 4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2D);
+
+MidColumnMatrix(array2D);
+
+
