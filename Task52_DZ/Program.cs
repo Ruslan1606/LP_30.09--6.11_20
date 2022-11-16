3f3e5bac3af9218b557d8a5e305ee7614a4791d0
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-11-17 01:44:45 +0500

    add 52

--- Task52_DZ/Program.cs
+++ Task52_DZ/Program.cs
@@ -9,6 +9,66 @@
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 
+// int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+// {
+//     var matrix = new int[rows, columns];
+//     var rnd = new Random();
+
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = rnd.Next(min, max + 1);
+//         }
+//     }
+//     return matrix;
+// }
+
+// int[,] MidColumnMatrix(int[,] array2D)
+// {
+//     double[] column;
+//     column = new double[array2D.GetLength(1)];
+//     for (int j = 0; j < array2D.GetLength(1); j++)
+//     {
+//         int sum;
+//         sum = 0;
+//          for (int i = 0; i < array2D.GetLength(0); i++)
+//         {
+//             sum += array2D[i, j];
+//         }
+//         double result = sum / array2D.GetLength(0);
+//         column[j] = result;
+//     }
+//     return array2D;
+// }
+
+
+// void PrintMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         Console.Write("|");
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4}, ");
+//             else Console.Write($"{matrix[i, j], 4} ");
+//         }
+//         Console.WriteLine("|");
+//     }
+// }
+
+// int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
+// PrintMatrix(array2D);
+
+// var result = MidColumnMatrix(array2D);
+
+// for (int i = 0; i < result.Length; i++)
+// {
+//     Console.WriteLine($"{[i]}:{result[i]}");
+// }
+
+
+
 int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
 {
     var matrix = new int[rows, columns];
@@ -24,16 +84,44 @@ int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
     return matrix;
 }
 
-void MidColumnMatrix(int[,] array2D)
+double[] ProcessArray(int[,] array2D) {
+  //Подготовка к циклам
+  double[] column;
+  column = new double[array2D.GetLength(1)];
+  
+  //Обработка столбцов
+  for (int j = 0; j < array2D.GetLength(1); j++)
+  {
+    int sum;
+    sum = 0;
+      //Обработка колонок
+      for (int i = 0; i < array2D.GetLength(0); i++)
+      {
+        sum += array2D[i,j];
+      }
+    double result = (double)sum / array2D.GetLength(0);
+    column[j] = result;
+  }
+
+  return column;
+}
+
+int[,] MidColumnMatrix(int[,] array2D)
 {
+  	int sum;  
+  	double[] column;
     for (int j = 0; j < array2D.GetLength(1); j++)
     {
-        int sum = 0;
+      	sum = 0;
         for (int i = 0; i < array2D.GetLength(0); i++)
         {
             sum += array2D[i, j];
         }
+      	double result = sum / array2D.GetLength(0);       
+        column = new double[array2D.GetLength(1)];
+        column[j] = result;   
     }
+    return array2D;    
 }
 
 
@@ -54,6 +142,8 @@ void PrintMatrix(int[,] matrix)
 int[,] array2D = CreateMatrixRndInt(3, 4, 1, 10);
 PrintMatrix(array2D);
 
-MidColumnMatrix(array2D);
+var result = ProcessArray(array2D);
 
-
+for (int i = 0; i < result.Length; i++) {
+  Console.WriteLine($"[{i}] = {result[i]} ");
+}
