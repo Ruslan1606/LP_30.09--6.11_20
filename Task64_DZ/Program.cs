25001ad9696b5d2347296139605f966105322b6a
«rus-nur@yandex.ru» <rus-nur@yandex.ru>
2022-11-26 01:57:35 +0500

    add 64 66 68

---
+++ Task64_DZ/Program.cs
@@ -0,0 +1,24 @@
+﻿//   Задача 64: Задайте значение N. Напишите программу, 
+//   которая выведет все натуральные числа в промежутке от N до 1. 
+//   Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNumber(int number)
+{
+    if (number > 1)
+    {
+        Console.Write($"{number}, ");
+        PrintNumber(number - 1);
+    }
+    else
+    {
+        Console.Write($"{number}");
+    }
+}
+
+Console.WriteLine("Введите число:");
+string key = Console.ReadLine();
+int number = Int32.Parse(key);
+PrintNumber(number);
---
+++ Task66_DZ/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumNumbers(int startNum, int endNum)
+{
+    int result = 0;
+    for (int i = startNum; i <= endNum; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число M:");
+string keyM = Console.ReadLine();
+int numberM = Int32.Parse(keyM);
+Console.WriteLine("Введите число N:");
+string keyN = Console.ReadLine();
+int numberN = Int32.Parse(keyN);
+var result = 0;
+if (numberM < numberN)
+{
+    result = SumNumbers(numberM, numberN);
+}
+else
+{
+    result = SumNumbers(numberN, numberM);
+}
+
+Console.WriteLine($"M = {numberM}; N = {numberN} -> {result}");
